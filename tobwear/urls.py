45d481f06908aa9f60dedfb83e0a7de155ec8cc4
MustafaKayra <m.toker2009@gmail.com>
2025-08-23 13:43:39 +0300

    Kullanıcı giriş sayfası eklendi

--- tobwear/urls.py
+++ tobwear/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from shop import views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about/',views.about,name="about"),
     path('products/',views.products,name="products"),
-    path('shoppingcard/',views.shoppingcart,name="shoppingcard")
+    path('shoppingcard/',views.shoppingcart,name="shoppingcard"),
+    path('login/',users_views.login,name="login")
 ]
