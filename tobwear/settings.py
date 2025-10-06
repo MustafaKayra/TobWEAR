bad17fb8b77f2c187a6a004826a8375d453a688c
MustafaKayra <m.toker2009@gmail.com>
2025-10-06 22:19:30 +0300

    static root düzenlendi

--- tobwear/settings.py
+++ tobwear/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i51-suy(v(ki0-95-4_zhgl)x9qbe_5way)ce+dipr2-9mnt)$"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'tobwear.wsgi.application'
 database_url = os.environ.get("DATABASE_URL")
 
 DATABASES = {
-    "default": dj_database_url.parse("postgresql://tobwear_postgresql_user:dNRRqRlIjxChbbVzFkeckqFLbFSzWY6y@dpg-d3gh01ggjchc739otdb0-a.oregon-postgres.render.com/tobwear_postgresql")
+    "default": dj_database_url.parse(database_url)
 }
 
 
@@ -128,17 +128,18 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 
 MEDIA_URL = '/media/'
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dllagrl4t',
-    'API_KEY': '287881725936462',
-    'API_SECRET': 'xOqwrzw8ShOnPUNCSRrkkQ_lKp4'
+    'API_KEY': '119298294657583',
+    'API_SECRET': 'WTY5WmYZZxhfX2zwozRAIyzSQ78'
 }
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
